4f4cab852cfa860211f55cf4c7b29b78fa51a7ca
sergii_omelianskyi <sergii_omelianskyi@bitbucket.org>
2022-02-03 17:13:30 +0200

    first commit

---
+++ src/main/java/edu/doggy228/loyaltyexch/lsemu/LSEmuApplication.java
@@ -0,0 +1,42 @@
+package edu.doggy228.loyaltyexch.lsemu;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.ExternalDocumentation;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories(basePackages = "edu.doggy228.loyaltyexch.lsemu")
+public class WMSApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WMSApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI lsemuOpenAPI() {
+		return new OpenAPI()
+				.info(new Info().title("Loyalty System (emu) API")
+						.description("Емуляція роботи систем лояльності.")
+						.version("v0.1.1")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")))
+				.components(new Components()
+						.addSecuritySchemes("bearer-key",
+								new SecurityScheme().type(SecurityScheme.Type.HTTP).scheme("bearer").bearerFormat("JWT").description("User access token.")));
+	}
+
+	@Bean
+	public GroupedOpenApi lsemuOpenApi() {
+		return GroupedOpenApi.builder()
+				.group("Loyalty System (emu) API")
+				.packagesToScan("edu.doggy228.loyaltyexch.wms.api.v1.lsemu")
+				.build();
+	}
+}
